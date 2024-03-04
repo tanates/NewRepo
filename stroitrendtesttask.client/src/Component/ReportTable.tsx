b19d30d8a5d1bc0389b022f0cdbc52a5122aa840
Tanates <timur876376.ru@mail.ru>
2024-03-05 01:15:46 +0700

    Поправил вывод таблицы Tags (надо понять почему в верстку он запихывает прошлы данные а новые не выводит )

--- stroitrendtesttask.client/src/Component/ReportTable.tsx
+++ stroitrendtesttask.client/src/Component/ReportTable.tsx
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './ReportStyle.css'
+import TagsTable from './TagsTable';
 interface ReportData {
   total: number;
   records: { [key: string]: { [key: string]: number } };
@@ -14,14 +16,14 @@ const API_URL = `https://localhost:7150/api/table/`
 
 const Table: React.FC<TableProps> = ({ reportName }) => {
   const [data, setData] = useState<ReportData | null>(null);
- 
+
   React.useEffect(() => {
     const fetchData = async () => {
       try {
         const result = await axios.get<ReportData>(API_URL+reportName);
         setData(result.data);
       } catch (error) {
-      
+
         const fallbackURL = `https://DockerPuth/${reportName}`
         if (fallbackURL) {
           const fallbackResult = await axios.get<ReportData>(fallbackURL);
@@ -31,7 +33,7 @@ const Table: React.FC<TableProps> = ({ reportName }) => {
         }
       }
     };
-  
+
     fetchData();
   }, [reportName, API_URL]);
 
@@ -39,42 +41,60 @@ const Table: React.FC<TableProps> = ({ reportName }) => {
     return <div>Loading...</div>;
   }
 
+  const tableHeaders = Object.entries(data.records[Object.keys(data.records)[0]]).map(([name, _]) => name);
+  const tableHeadersTags = Object.entries(data.records[Object.keys(data.records)[0]]).map(([name, _]) => name);
   const tableRows = Object.entries(data.records).map(([date, records]) => {
+    const rowData = [date, Object.values(records).reduce((total, value) => total + value, 0), ...Object.values(records)];
+
     return (
       <tr key={date}>
-        <td>{date}</td>
-        <td>{Object.values(records).reduce((total, value) => total + value, 0)}</td>
-        {Object.entries(records).map(([name, value]) => (
-          <td key={name}>{value}</td>
+        {rowData.map((value, index) => (
+          <td key={`${date}-${index}`}>{value}</td>
         ))}
       </tr>
     );
   });
 
+  if(reportName!=="Tags"){
   return (
-
-    <div className='row'>
-        <div className="col-12">
-          <div className='card'>
-            <div className="table-responsive">
-                <table className='table table-striped table-dark'>
-                  <thead className='bg-dark'>
-                    <tr className='bg-dark'>
-                      <th scope="col">Date</th>
-                      <th scope="col">Total</th>
-                        {Object.entries(data.records[Object.keys(data.records)[0]]).map(([name, _]) => (
-                        <th scope="col" key={name}>{name}</th>
-                      ))}
-                    </tr>
-                  </thead>
-                  <tbody className="customtable">
-                    {tableRows}
-                  </tbody>
-                </table>
-          </div>
+    <div className="content">
+      <div className='container'>
+        <div className="table-responsive">
+          <table className='table table-striped table-bordered custom-table'>
+            <thead>
+              {reportName!=="Tags"&&(
+              <tr>
+                <th scope="col">Data</th>
+                <th scope="col">Total</th>
+                {tableHeaders.map((name) =>(
+                    
+                     <th scope="col" key={name}>{name}</th>
+                ))}
+              </tr>
+              )}
+              {reportName==="Tags"&&(
+                  tableHeadersTags
+              )}
+            </thead>
+            <tbody>
+              { reportName === "Tags" && (
+                 tableRows
+              )} 
+              {reportName!=="Tags"&&(
+                tableRows
+              )}
+            </tbody>
+          </table>
         </div>
       </div>
-   </div>
+    </div>
   );
+              }
+    else{
+      return (
+        <TagsTable reportData={data}/>
+      )
+    }
 };
-export default Table 
+
+export default Table;
---
+++ stroitrendtesttask.client/src/Component/TagsTable.tsx
@@ -0,0 +1,52 @@
+interface Record {
+    [key: string]: number;
+  }
+  
+  interface ReportData {
+    records: {
+      [key: string]: Record;
+    };
+  }
+  
+  interface TableProps {
+    reportData: ReportData;
+  }
+  
+  const TagsTable: React.FC<TableProps> = ({ reportData }) => {
+    const { records } = reportData;
+    const dates = Object.keys(records);
+    const values = Object.values(records)[0];
+    const keys = Object.keys(values);
+
+  return (
+    <div className="content">
+      <div className='container'>
+        <div className="table-responsive">
+          <table className='table table-striped table-bordered custom-table'>
+                <thead >
+                    <tr>
+                    <th>Дата</th>
+                    <th>Значения</th>
+                    </tr>
+                </thead>
+                <tbody >
+                    {dates.map((date) => (
+                    <tr key={date}>
+                        <td>{date}</td>
+                        <td >
+                        {keys.map((key) => (
+          <div className="d-flex" key={`${date}-${key}`}>
+            {key} - {records[date][key] || 0}
+          </div>
+        ))}
+                        </td>
+                    </tr>
+                    ))}
+                </tbody>
+            </table>
+       </div>    
+    </div>
+</div>
+  );
+};
+export default TagsTable
